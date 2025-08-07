a78900f0d84b3c67b26b6bfec81f92a787db4d21
Dev_Brandon_David_Gonzalez <gonzdeiv123@gmail.com>
2025-08-07 17:32:33 -0500

    creation of backend UTS, microservice Users

---
+++ uts-backend/src/main/java/com/example/uts_backend/UtsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.uts_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtsBackendApplication.class, args);
+	}
+
+}
---
+++ uts-backend/src/test/java/com/example/uts_backend/UtsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uts_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
